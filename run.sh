71f937dd1a7520fb56698c108be08a225af91427
go49daq <baicheng.lin@tum.de>
2025-07-04 02:22:01 +0200

    update

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e  # Exit on any error
 
-# 1. Navigate to the DREAMPlace_TUM directory
-cd /work/DREAMPlace_TUM/install
+# 1. Navigate to the DREAMPlace_TUM install directory
+cd /work/DREAMPlace_TUM/install || { echo "Directory not found!"; exit 1; }
 
+# 2. Run placement using INSTA configuration
 python dreamplace/Placer.py params_insta.json
 
